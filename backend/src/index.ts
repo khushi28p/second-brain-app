fc0d933b0d2b3529b60911132612b569b622897e
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-05-12 17:24:21 +0530

    signin endpoint aded

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,64 +1,67 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
-import { UserModel } from './db';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import { UserModel } from "./db";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const port = process.env.PORT;
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-const jwtSecret = process.env.JWT_SECRET;
+app.post("/api/v1/signup", async (req, res) => {
+  const { username, password } = req.body;
 
-app.post("/api/v1/signup", async(req, res) => {
-    const {username, password} = req.body;
+  try {
+    await UserModel.create({
+      username,
+      password,
+    });
 
-    try{
-        await UserModel.create({
-        username, 
-        password
-    })
+    res.json({ message: "User created" });
+  } catch (error) {
+    res.status(400).json({ message: "User already exists" });
+  }
+});
 
-    res.json({message: "User created"})
-    } catch(error){
-        res.status(400).json({message: "User already exists"})
-    }
-})
+app.post("/api/v1/signin", async (req, res) => {
+  const { username, password } = req.body;
 
-app.post("/api/v1/signin", async(req, res) => {
-    const {username,  password} = req.body;
-
+  try {
     const existingUser = await UserModel.findOne({
-        username,
-        password
-    })
+      username,
+      password,
+    });
 
-    
-})
+    if (existingUser) {
+      const token = jwt.sign(
+        {
+          id: existingUser._id,
+        },
+        process.env.JWT_SECRET!
+      );
 
-app.post("/api/v1/content", async(req, res) => {
-    
-})
+      res.json({ token });
+    } else {
+      res.status(403).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
-app.get("/api/v1/content", async(req, res) => {
-    
-})
+app.post("/api/v1/content", async (req, res) => {});
 
-app.delete("/api/v1/content", async(req, res) => {
-    
-})
+app.get("/api/v1/content", async (req, res) => {});
 
-app.post("/api/v1/brain/share", async(req, res) => {
+app.delete("/api/v1/content", async (req, res) => {});
 
-})
+app.post("/api/v1/brain/share", async (req, res) => {});
 
-app.get("/api/v1/brain/:shareLink", async(req, res) => {
+app.get("/api/v1/brain/:shareLink", async (req, res) => {});
 
-})
-
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });

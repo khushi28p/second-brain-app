924f6cbe53ac892d81a59701ad22d095dfc34358
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-05-13 20:08:00 +0530

    aded some ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-import { Button } from "./components/ui/Button.tsx"
+import { Button } from "./components/ui/Button"
+import { PlusIcon } from "./components/icons/PlusIcon"
+import { ShareIcon } from "./components/icons/ShareIcon"
 
 function App() {
 
+  const handleShare = () => {
+    console.log("Share button clicked")
+  }
+
+  const handleAddContent = () => {
+    console.log("Add Content button clicked")
+  }
+
   return (
     <>
-      <Button variant="primary" size="md" text="Share" />
+      <div className="flex justify-end">
+        <Button variant="secondary" text="Share" size="md" startIcon={<ShareIcon size="md" />} onClick={handleShare} />
+      <Button variant="primary" text="Add Content" size="md" startIcon={<PlusIcon size="md" />} onClick={handleAddContent} />
+      </div>
     </>
   )
 }
--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,12 +1,29 @@
+import { type ReactElement } from "react";
+
+type ButtonVariants = "primary" | "secondary";
+
 export interface ButtonProps{
-    variant: "primary" | "secondary",
+    variant: ButtonVariants,
     size: "sm" | "md" | "lg",
     text: string,
-    startIcon?: any,
-    endIcon?: any,
+    startIcon?: ReactElement,
+    endIcon?: ReactElement,
     onClick: () => void;
 }
 
+const variantStyles = {
+    "primary": "bg-blue-700 text-white",
+    "secondary": "bg-blue-400 text-white",
+}
+
+const sizeStyles = {
+    "sm": "px-2 py-1 text-sm",  
+    "md": "px-4 py-2 text-md",
+    "lg": "px-6 py-3 text-lg",
+}
+
+const defaultStyles = "rounded-md p-4 flex items-center justify-center "
+
 export const Button = (props: ButtonProps) => {
-    return <button></button>
+return <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>{props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null} {props.text} {props.endIcon}</button>
 }

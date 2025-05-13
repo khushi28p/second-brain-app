e1cdde649f66943e7da73190b40f4f9bf9ba180c
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-05-13 15:56:54 +0530

    frontend starter code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/Button.tsx"
+
+function App() {
+
+  return (
+    <>
+      <Button variant="primary" size="md" text="Share" />
+    </>
+  )
+}
+
+export default App
